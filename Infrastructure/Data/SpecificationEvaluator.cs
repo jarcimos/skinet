3be45776cea2fec8b87fa60041ead685bc5089a8
iaromos <69873911+jarcimos@users.noreply.github.com>
2025-02-18 02:03:53 -0500

    End of section 5

--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -28,10 +28,10 @@ public class SpecificationEvaluator<T> where T : BaseEntity
             query = query.Distinct();
         }
 
-        // if (spec.IsPagingEnabled)
-        // {
-        //     query = query.Skip(spec.Skip).Take(spec.Take);
-        // }
+        if (spec.IsPagingEnabled)
+        {
+            query = query.Skip(spec.Skip).Take(spec.Take);
+        }
 
         // query = spec.Includes.Aggregate(query, (current, include) => current.Include(include));
         // query = spec.IncludeStrings.Aggregate(query, (current, include) => current.Include(include));
@@ -69,10 +69,10 @@ public class SpecificationEvaluator<T> where T : BaseEntity
             selectQuery = selectQuery?.Distinct();
         }
 
-        // if (spec.IsPagingEnabled)
-        // {
-        //     selectQuery = selectQuery?.Skip(spec.Skip).Take(spec.Take);
-        // }
+        if (spec.IsPagingEnabled)
+        {
+            selectQuery = selectQuery?.Skip(spec.Skip).Take(spec.Take);
+        }
 
         return selectQuery ?? query.Cast<TResult>();
     }
