7f2a2fa1645bc14595d53dde6797dca4c393b00e
iaromos <69873911+jarcimos@users.noreply.github.com>
2025-02-19 00:40:59 -0500

    End of section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [ HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  baseUrl = 'http://localhost:5019/api/';
+  private http = inject(HttpClient);
   title = 'client';
+  products: Product[] = [];
+
+  ngOnInit(): void {
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
+      next: response => this.products = response.data,
+      error: error => console.error(error),
+      complete: () => console.log('complete')
+    })
+  }
 }
--- client/src/app/layout/header/header.component.ts
+++ client/src/app/layout/header/header.component.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
+import {MatButton} from '@angular/material/button';
+import{MatBadge} from '@angular/material/badge';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [
+    MatBadge, MatButton, MatIcon
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
